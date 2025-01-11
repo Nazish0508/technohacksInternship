cc1381347cf68b3e348af4053aed4a4b68e04b07
Nazish Ansari <ansarinaz.2104@gmail.com>
2025-01-11 21:25:08 +0530

    Re-add frontend folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-app-bar app color="deep-purple" dark>
+      <div class="d-flex slign-center"></div>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <todoComp />
+</template>
+
+<script>
+import todoComp from "../components/todoComp";
+
+export default {
+  name: "HomeView",
+
+  components: {
+    todoComp,
+  },
+};
+</script>
